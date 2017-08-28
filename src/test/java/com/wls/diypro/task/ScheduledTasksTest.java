6c85e9556f8ee2f9546930bf93958f1bb5514557
longshanw <longshanw@users.noreply.github.com>
2017-08-28 11:49:30 +0800

    增加jdbctemplete支持

---
+++ src/test/java/com/wls/diypro/task/ScheduledTasksTest.java
@@ -0,0 +1,31 @@
+package com.wls.diypro.task;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ScheduledTasksTest {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Autowired
+    private ScheduledTasks scheduledTasks;
+
+    @Test
+    public void reportCurrentTime() throws Exception {
+        scheduledTasks.reportCurrentTime();
+    }
+
+}
