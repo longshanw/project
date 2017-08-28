de5396ee3ad6568b5786f7d573460f8b7f4f6e69
longshanw <longshanw@users.noreply.github.com>
2017-08-28 14:14:20 +0800

    提交人：王龙山 摘要描述：增加jdbctemplete双数据源支持 时间：2017年8月28日13:54:43

--- src/main/java/com/wls/diypro/service/impl/OrderInfoServiceImpl.java
+++ src/main/java/com/wls/diypro/service/impl/OrderInfoServiceImpl.java
@@ -10,6 +10,7 @@ import com.wls.diypro.service.IOrderInfoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -29,6 +30,7 @@ public class OrderInfoServiceImpl implements IOrderInfoService {
     @Autowired
     private OrderInfoMapper orderInfoMapper;
 
+    @Qualifier("orderInfoDaoImpl")
     @Autowired
     private IOrderInfoDao iOrderInfoDao;
 
