19752251744578753b62ec91eb336498a2a5e82f
longshanw <longshanw@users.noreply.github.com>
2017-09-04 19:03:39 +0800

    提交人：王龙山 查询erp渠道中药商品信息 时间：2017年9月4日14:40:11

---
+++ src/test/java/com/wls/integrateplugs/jpa/JpaTest.java
@@ -0,0 +1,49 @@
+package com.wls.integrateplugs.jpa;
+
+import com.wls.integrateplugs.jpa.model.primary.User;
+import com.wls.integrateplugs.jpa.model.primary.UserRepository;
+import com.wls.integrateplugs.jpa.model.second.Message;
+import com.wls.integrateplugs.jpa.model.second.MessageRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class JpaTest {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test() throws Exception {
+
+        userRepository.save(new User("aaa", 10));
+        userRepository.save(new User("bbb", 20));
+        userRepository.save(new User("ccc", 30));
+        userRepository.save(new User("ddd", 40));
+        userRepository.save(new User("eee", 50));
+
+        Assert.assertEquals(5, userRepository.findAll().size());
+
+        messageRepository.save(new Message("o1", "aaaaaaaaaa"));
+        messageRepository.save(new Message("o2", "bbbbbbbbbb"));
+        messageRepository.save(new Message("o3", "cccccccccc"));
+
+        Assert.assertEquals(3, messageRepository.findAll().size());
+
+    }
+
+
+}
